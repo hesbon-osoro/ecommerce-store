50e786bdf3022b54f00657289ae5daacd0886e3a
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-09-02 15:47:34 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './store';
+import { positions, transitions, Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
+
+const options = {
+	timeout: 5000,
+	position: positions.BOTTOM_CENTER,
+	transition: transitions.SCALE,
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+	<Provider store={store}>
+		<AlertProvider template={AlertTemplate} {...options}>
+			<App />
+		</AlertProvider>
+	</Provider>
 );
-
