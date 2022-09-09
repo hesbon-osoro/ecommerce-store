d5568cb42e09a73a16c8a8abc7de0bae1cd096f9
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-09-10 01:00:36 +0300

    OrderList.jsx

---
+++ src/component/Admin/OrderList.jsx
@@ -0,0 +1,123 @@
+import React, { Fragment, useEffect } from 'react';
+import { DataGrid } from '@material-ui/data-grid';
+import './productList.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { useAlert } from 'react-alert';
+import { Button } from '@material-ui/core';
+import MetaData from '../layout/MetaData';
+import { Edit, Delete } from '@material-ui/icons';
+import Sidebar from './Sidebar';
+import { deleteOrder, getAllOrders, clearErrors } from '../../actions/order';
+import { DELETE_ORDER_RESET } from '../../constants/order';
+
+const OrderList = ({ history }) => {
+	const dispatch = useDispatch();
+	const alert = useAlert();
+	const { error, orders } = useSelector(state => state.allOrders);
+	const { error: deleteError, isDeleted } = useSelector(state => state.order);
+
+	const deleteOrderHandler = id => {
+		dispatch(deleteOrder(id));
+	};
+	useEffect(() => {
+		if (error) {
+			alert.error(error);
+			dispatch(clearErrors());
+		}
+		if (deleteError) {
+			alert.error(deleteError);
+			dispatch(clearErrors());
+		}
+		if (isDeleted) {
+			alert.success('Order Deleted Successfully');
+			history.push('/admin/orders');
+			dispatch({ type: DELETE_ORDER_RESET });
+		}
+		dispatch(getAllOrders());
+	}, [dispatch, alert, error, deleteError, history, isDeleted]);
+
+	const columns = [
+		{ field: 'id', headerName: 'Order ID', minWidth: 300, flex: 1 },
+		{
+			field: 'status',
+			headerName: 'Status',
+			minWidth: 150,
+			flex: 0.5,
+			cellClassName: params => {
+				return params.getValue(params.id, 'status') === 'Delivered'
+					? 'greenColor'
+					: 'redColor';
+			},
+		},
+		{
+			field: 'itemsQty',
+			headerName: 'Items Qty',
+			type: 'number',
+			minWidth: 150,
+			flex: 0.4,
+		},
+		{
+			field: 'amount',
+			headerName: 'Amount',
+			type: 'number',
+			minWidth: 270,
+			flex: 0.5,
+		},
+		{
+			field: 'actions',
+			flex: 0.3,
+			headerName: 'Actions',
+			minWidth: 150,
+			type: 'number',
+			sortable: false,
+			renderCell: params => {
+				return (
+					<Fragment>
+						<Link to={`/admin/order/${params.getValue(params.id, 'id')}`}>
+							<Edit />
+						</Link>
+						<Button
+							onClick={() =>
+								deleteOrderHandler(params.getValue(params.id, 'id'))
+							}
+						>
+							<Delete />
+						</Button>
+					</Fragment>
+				);
+			},
+		},
+	];
+	const rows = [];
+	orders &&
+		orders.forEach(order => {
+			rows.push({
+				id: order._id,
+				itemsQty: order.orderItems.length,
+				amount: order.totalPrice,
+				status: order.orderStatus,
+			});
+		});
+	return (
+		<Fragment>
+			<MetaData title={'ALL ORDERS - Admin'} />
+			<div className="dashboard">
+				<Sidebar />
+				<div className="productListContainer">
+					<h1 id="productListHeading">ALL ORDERS</h1>
+					<DataGrid
+						rows={rows}
+						columns={columns}
+						pageSize={10}
+						disableSelectionOnClick
+						className="productListTable"
+						autoHeight
+					/>
+				</div>
+			</div>
+		</Fragment>
+	);
+};
+
+export default OrderList;
