ffa147139f149c342d0e0b44c8809b0f8d1196ad
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-09-10 01:00:24 +0300

    Dashboard.jsx

---
+++ src/component/Admin/Dashboard.jsx
@@ -0,0 +1,97 @@
+import React, { useEffect } from 'react';
+import Sidebar from './Sidebar';
+import './dashboard.css';
+import { Typography } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { Doughnut, Line } from 'react-chartjs-2';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAdminProduct } from '../../actions/product';
+import { getAllOrders } from '../../actions/order';
+import { getAllUsers } from '../../actions/user';
+import MetaData from '../layout/MetaData';
+
+const Dashboard = () => {
+	const dispatch = useDispatch();
+	const { products } = useSelector(state => state.products);
+	const { orders } = useSelector(state => state.allOrders);
+	const { users } = useSelector(state => state.allUsers);
+	let outOfStock = 0;
+
+	products &&
+		products.forEach(product => {
+			if (product.Stock === 0) {
+				outOfStock += 1;
+			}
+		});
+	useEffect(() => {
+		dispatch(getAdminProduct());
+		dispatch(getAllOrders());
+		dispatch(getAllUsers());
+	}, [dispatch]);
+	let totalAmount = 0;
+	orders &&
+		orders.forEach(order => {
+			totalAmount += order.totalPrice;
+		});
+	const lineState = {
+		labels: ['Initial Amount', 'Amount Earned'],
+		datasets: [
+			{
+				label: 'TOTAL AMOUNT',
+				backgroundColor: ['tomato'],
+				hoverBackgroundColor: ['rgb(197, 72, 49)'],
+				data: [0, totalAmount],
+			},
+		],
+	};
+	const doughnutState = {
+		labels: ['Out of Stock', 'InStock'],
+		datasets: [
+			{
+				backgroundColor: ['#00A6B4', '#6800B4'],
+				hoverBackgroundColor: ['#4B5000', '#35014F'],
+				data: [outOfStock, products.length - outOfStock],
+			},
+		],
+	};
+
+	return (
+		<div className="dashboard">
+			<MetaData title={'Admin Dashboard'} />
+			<Sidebar />
+			<div className="dashboardContainer">
+				<Typography component="h1">Dashboard</Typography>
+				<div className="dashboardSummary">
+					<div>
+						<p>
+							Total Amount <br />
+							Ksh.{totalAmount}
+						</p>
+					</div>
+					<div className="dashboardSummaryBox2">
+						<Link to="/admin/products">
+							<p>Product</p>
+							<p>{products && products.length}</p>
+						</Link>
+						<Link to="/admin/orders">
+							<p>Orders</p>
+							<p>{orders && orders.length}</p>
+						</Link>
+						<Link to="/admin/users">
+							<p>Users</p>
+							<p>{users && users.length}</p>
+						</Link>
+					</div>
+				</div>
+				<div className="lineChart">
+					<Line data={lineState} />
+				</div>
+				<div className="doughnutChart">
+					<Doughnut data={doughnutState} />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Dashboard;
