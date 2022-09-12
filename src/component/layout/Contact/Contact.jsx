3ab999713a88f5c694cc6b29f34dbd7a05155e92
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-09-12 21:55:13 +0300

    Contact.jsx

---
+++ src/component/layout/Contact/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './contact.css';
+import { Button } from '@material-ui/core';
+
+const Contact = () => {
+	return (
+		<div className="contactContainer">
+			<a href="mailto:hesbonosoro1@gmail.com" className="mailBtn">
+				<Button> Contact: hesbonosoro1@gmail.com</Button>
+			</a>
+		</div>
+	);
+};
+
+export default Contact;
