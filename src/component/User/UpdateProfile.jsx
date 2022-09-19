c1968d2c18a1940e927e19bb3df4534509fa3389
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-09-19 03:19:54 +0300

    UpdateProfile.jsx

---
+++ src/component/User/UpdateProfile.jsx
@@ -0,0 +1,115 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import './updatePassword.css';
+import Loader from '../layout/Loader/Loader';
+import { MailOutline, Face } from '@material-ui/icons';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearErrors, updateProfile, loadUser } from '../../actions/user';
+import { useAlert } from 'react-alert';
+import { UPDATE_PROFILE_RESET } from '../../constants/user';
+import MetaData from '../layout/MetaData';
+
+const UpdateProfile = ({ history }) => {
+  const dispatch = useDispatch();
+  const alert = useAlert();
+  const { user } = useSelector(state => state.user);
+  const { error, isUpdated, loading } = useSelector(state => state.profile);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [avatar, setAvatar] = useState('');
+  const [avatarPreview, setAvatarPreview] = useState('/Profile.png');
+  const updateProfileSubmit = e => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.set('name', name);
+    formData.set('email', email);
+    formData.set('avatar', avatar);
+    dispatch(updateProfile(formData));
+  };
+  const updateProfileDataChange = e => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (reader.readyState === 2) {
+        setAvatarPreview(reader.result);
+        setAvatar(reader.result);
+      }
+    };
+    reader.readAsDataURL(e.target.files[0]);
+  };
+  useEffect(() => {
+    if (user) {
+      setName(user.name);
+      setEmail(user.email);
+      setAvatarPreview(user.avatar.url);
+    }
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+    if (isUpdated) {
+      alert.success('Profile updated successfully');
+      dispatch(loadUser());
+      history.push('/account');
+      dispatch({ type: UPDATE_PROFILE_RESET });
+    }
+  }, [dispatch, alert, error, isUpdated, history, user]);
+  return (
+    <Fragment>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <MetaData title={'Update Profile'} />
+          <div className="updateProfileContainer">
+            <div className="updateProfileBox">
+              <h2 className="updateProfileHeading">Update Profile</h2>
+              <form
+                className="updateProfileForm"
+                encType="multipart/form-data"
+                onSubmit={updateProfileSubmit}
+              >
+                <div className="updateProfileName">
+                  <Face />
+                  <input
+                    type="text"
+                    placeholder="Name"
+                    required
+                    name="name"
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                  />
+                </div>
+                <div className="updateProfileEmail">
+                  <MailOutline />
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    required
+                    name="email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                  />
+                </div>
+                <div id="updateProfileImage">
+                  <img src={avatarPreview} alt="Avatar Preview" />
+                  <input
+                    type="file"
+                    name="avatar"
+                    accept="image/*"
+                    onChange={updateProfileDataChange}
+                  />
+                </div>
+                <input
+                  type="submit"
+                  value="Update"
+                  className="updateProfileBtn"
+                />
+              </form>
+            </div>
+          </div>
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
+
+export default UpdateProfile;
