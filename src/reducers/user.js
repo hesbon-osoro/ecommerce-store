86e66530d1f1e189cbb9cbe4567b2b7cc0ca9979
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-09-23 00:44:21 +0300

    update fix

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -220,7 +220,7 @@ export const allUsersReducer = (state = { users: [] }, action) => {
   }
 };
 
-export const usersDetailsReducer = (state = { user: {} }, action) => {
+export const userDetailsReducer = (state = { user: {} }, action) => {
   switch (action.type) {
     case USER_DETAILS_REQUEST:
       return {
